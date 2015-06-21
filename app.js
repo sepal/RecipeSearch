28e098549d26ad8cb8440a00b1f819d6e435744f
Sebastian <sep.gil@gmail.com>
2015-06-21 22:03:40 +0200

    Added basic boilerplate code.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var middleware = require('./source/middleware');
 var config = require('./src/utils/config');
 var logger = require('./src/utils/logger');
 
@@ -8,7 +9,8 @@ var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-
+require('./source/api')(app);
+app.use(middleware.errors());
 
 app.listen(config.port, function () {
   logger.info('App listening on port ' + config.port);
---
+++ src/middleware/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  errors: require('./errors'),
+  db: {
+  }
+};
