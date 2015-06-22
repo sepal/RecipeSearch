04bad1011bfd910f2af8bc172b391bc05b3d9e11
Sebastian <sep.gil@gmail.com>
2015-06-22 22:55:47 +0200

    Implemented basic handlebars handling.

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import handlebars from 'express-handlebars';
 
 import middleware from './src/middleware';
 import config from './src/utils/config';
 import logger from './src/utils/logger';
 
 var app = express();
+app.engine('.hbs', handlebars({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
@@ -17,3 +20,8 @@ app.use(middleware.errors());
 app.listen(config.port, function () {
   logger.info('App listening on port ' + config.port);
 });
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
