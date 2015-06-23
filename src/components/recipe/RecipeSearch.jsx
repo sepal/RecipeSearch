2123f04d3a93d78df053ef98c49b9fe37d70d548
Sebastian <sep.gil@gmail.com>
2015-06-24 00:53:15 +0200

    Improved the header search form.

--- src/components/recipe/RecipeSearch.jsx
+++ src/components/recipe/RecipeSearch.jsx
@@ -6,7 +6,7 @@ class RecipeSearch extends React.Component {
       <div className="search search--recipe">
         <form accept="UTF-8" method="get" action={this.props.target}>
           <input type="text" className="text text--search" maxLength="128"
-                 name="search"
+                 name="search" size="50"
                  placeholder="An ingredient, recipe type, or just anything"
                  value={this.props.search} >
           </input>
