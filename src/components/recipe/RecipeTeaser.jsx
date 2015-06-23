50a42bd41830e167e06008f3568696d7a4c05d6d
Sebastian <sep.gil@gmail.com>
2015-06-24 00:04:35 +0200

    Changed the teaser text a bit.

--- src/components/recipe/RecipeTeaser.jsx
+++ src/components/recipe/RecipeTeaser.jsx
@@ -4,11 +4,11 @@ class RecipeTeaser extends React.Component {
   render() {
     return (
       <div className="recipe recipe--teaser">
-        <div className="recipe__name">
+        <h2 className="recipe__name">
           <a href={this.props.url}>{this.props.name}</a>
-        </div>
+        </h2>
         <div className="recipe__source">
-          <a href={this.props.source_url} target="_blank">{this.props.source}</a>
+          As found on <a href={this.props.source_url} target="_blank">{this.props.source}</a>
         </div>
       </div>
     )
