ae01b57916aefc994c368a80050476037cd8f7da
Sebastian <sep.gil@gmail.com>
2015-06-24 21:22:48 +0200

    Added empty recipeController.

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,7 +1,9 @@
 import front from './front'
 import searchController from './searchController'
+import recipeController from './recipeController'
 
 module.exports = function(app) {
   app.get('/', front);
-  app.get('/search', searchController.search)
+  app.get('/search', searchController.search);
+  app.get('/recipe/:id', recipeController.read);
 };
